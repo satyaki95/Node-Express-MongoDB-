d9d9c22d2678244c86e353a4bbe88a66c826adfb
satyaki95 <satyaki95@outlook.com>
2022-12-14 15:21:55 +0530

    node

---
+++ Local module - exports, require/Student.js
@@ -0,0 +1,16 @@
+const getName = () =>{
+    return "Satyaki";
+}
+
+const getAge = () =>{
+    return "28";
+}
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+
+
+module.exports = {
+    getAge,
+    getName
+}
