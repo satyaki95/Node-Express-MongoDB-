017350aa42048c5ad590af1dbf50fe6e377332d6
satyaki95 <satyaki95@outlook.com>
2023-01-06 18:31:59 +0530

    update

---
+++ 2 Fs module - managing files/index.js
@@ -0,0 +1,51 @@
+console.log('HI');
+
+const fs = require('fs');
+
+fs.writeFile('demo.txt',"my name is satyaki", (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("successfull");
+    }
+})
+
+fs.appendFile('demo.txt'," i am 28 year old", (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("successfull");
+    }
+})
+
+fs.readFile('demo.txt','utf-8', (err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+})
+
+fs.rename('demo.txt','hallo.txt', (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("successful");
+    }
+})
+
+fs.unlink('hallo.txt', (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("successful");
+    }
+})
+
+fs.exists('hallo.txt', (err)=>{
+    if(err){
+        console.log("found");
+    }else{
+        console.log("not found");
+    }
+})
